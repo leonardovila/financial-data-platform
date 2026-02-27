d981b15ad8762e617eb19f832ceb4513492278dd
Leonardo Enrique Vila <leonardovila.animation@gmail.com>
2026-02-27 17:02:50 -0300

    chore(api): enable CORS for landing consumption

--- financial_data_etl/api/app.py
+++ financial_data_etl/api/app.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from financial_data_etl.api.db import get_connection
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="financial-data-etl api")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
