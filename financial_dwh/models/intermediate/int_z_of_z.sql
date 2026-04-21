ea9a4757cc707c6d34c284ca6a7c96bdf2b1942b
Leonardo Enrique Vila <leonardovila.animation@gmail.com>
2026-04-21 02:30:21 -0300

    chore(frontend): refresh anomalies snapshot al 2026-04-20

--- financial_dwh/models/intermediate/int_z_of_z.sql
+++ financial_dwh/models/intermediate/int_z_of_z.sql
@@ -19,20 +19,16 @@
 --   FROM fact_derived_metrics
 --   WHERE date = CURRENT_DATE() AND ABS(z_of_z_rsi_14) > 2
 --   ORDER BY ABS(z_of_z_rsi_14) DESC
-
 {% macro z_of_z(col) -%}
     SAFE_DIVIDE(
         {{ col }} - AVG({{ col }}) OVER w_date,
         NULLIF(STDDEV({{ col }}) OVER w_date, 0)
     )
 {%- endmacro %}
-
 {{ config(materialized='view') }}
-
 SELECT
     i.symbol,
     i.date,
-
     -- Crudas (passthrough para que fact_derived_metrics no tenga que joinear más)
     i.open, i.high, i.low, i.close, i.volume,
     i.range_intraday,
@@ -41,7 +37,6 @@ SELECT
     i.rsi_14,
     i.sma_20_gap, i.sma_50_gap, i.sma_200_gap,
     i.high_dist_1m, i.high_dist_1y,
-
     -- Z intra-asset (passthrough)
     i.z_intra_ret_1d,
     i.z_intra_ret_1m,
@@ -52,7 +47,6 @@ SELECT
     i.z_intra_sma_200_gap,
     i.z_intra_range_intraday,
     i.z_intra_high_dist_1y,
-
     -- Z cross-asset (passthrough)
     c.z_cross_ret_1d,
     c.z_cross_ret_1m,
@@ -63,7 +57,6 @@ SELECT
     c.z_cross_sma_200_gap,
     c.z_cross_range_intraday,
     c.z_cross_high_dist_1y,
-
     -- Z OF Z: z-score del z_intra contra el universo del día
     {{ z_of_z('i.z_intra_ret_1d')         }} AS z_of_z_ret_1d,
     {{ z_of_z('i.z_intra_ret_1m')         }} AS z_of_z_ret_1m,
@@ -74,7 +67,6 @@ SELECT
     {{ z_of_z('i.z_intra_sma_200_gap')    }} AS z_of_z_sma_200_gap,
     {{ z_of_z('i.z_intra_range_intraday') }} AS z_of_z_range_intraday,
     {{ z_of_z('i.z_intra_high_dist_1y')   }} AS z_of_z_high_dist_1y
-
 FROM {{ ref('int_intra_asset_zscores') }} i
 LEFT JOIN {{ ref('int_cross_asset_zscores') }} c
   ON i.symbol = c.symbol AND i.date = c.date
